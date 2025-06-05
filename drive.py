3da9a73a571500a3034e31658e4efc5df9dac309
S-Marlon <marlovcbol.com.br@gmail.com>
2025-06-05 13:16:08 -0300

    Criação automatica do feed Rss

--- drive.py
+++ drive.py
@@ -56,24 +56,37 @@ def upload_arquivo(caminho_local, nome_no_drive):
     return file.get('id')
 
 
-def listar_audios_da_pasta():
+# def listar_audios_da_pasta():
+#     creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+#     service = autenticar_drive()
+
+#     query = f"'{PASTA_ID}' in parents and mimeType='audio/mpeg'"
+#     resultados = service.files().list(q=query, fields="files(id, name, createdTime)").execute()
+#     arquivos = resultados.get('files', [])
+
+#     episodios = []
+#     for arquivo in arquivos:
+#         episodios.append({
+#             'titulo': os.path.splitext(arquivo['name'])[0],
+#             'descricao': f"Episódio gerado automaticamente: {arquivo['name']}",
+#             'data': arquivo['createdTime'],
+#             'url': f"https://drive.google.com/uc?export=download&id={arquivo['id']}"
+#         })
+
+       
+#         return episodios
+
+
+def listar_arquivos():
     creds = Credentials.from_authorized_user_file('token.json', SCOPES)
     service = autenticar_drive()
 
-    query = f"'{PASTA_ID}' in parents and mimeType='audio/mpeg'"
-    resultados = service.files().list(q=query, fields="files(id, name, createdTime)").execute()
-    arquivos = resultados.get('files', [])
+    results = service.files().list(
+        q=f"'{PASTA_ID}' in parents and mimeType contains 'audio/' and trashed = false",
+        fields="files(id, name, createdTime)",
+        orderBy="createdTime desc"
+    ).execute()
+    return results.get('files', [])
 
-    episodios = []
-    for arquivo in arquivos:
-        episodios.append({
-            'titulo': os.path.splitext(arquivo['name'])[0],
-            'descricao': f"Episódio gerado automaticamente: {arquivo['name']}",
-            'data': arquivo['createdTime'],
-            'url': f"https://drive.google.com/uc?export=download&id={arquivo['id']}"
-        })
-
-       
-        return episodios
 
 
